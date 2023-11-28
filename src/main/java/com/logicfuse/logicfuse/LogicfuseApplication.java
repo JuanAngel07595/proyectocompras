1d92cd0b8ff3843dc5d39568b4674c48d2a558fc
Fabián Adrián Henao Tangarife <40705995+fab-henao@users.noreply.github.com>
2023-11-27 21:35:27 -0500

    Update LogicfuseApplication.java

--- src/main/java/com/logicfuse/logicfuse/LogicfuseApplication.java
+++ src/main/java/com/logicfuse/logicfuse/LogicfuseApplication.java
@@ -3,7 +3,12 @@ package com.logicfuse.logicfuse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication   
+@EnableAutoConfiguration
+@ComponentScan(basePackages={"com.logicfuse.logicfuse"})
+@EnableJpaRepositories(basePackages="com.logicfuse.logicfuse.repositories")
+@EnableTransactionManagement
+@EntityScan(basePackages="com.logicfuse.logicfuse.models")	
 public class LogicfuseApplication {
 
 	public static void main(String[] args) {
