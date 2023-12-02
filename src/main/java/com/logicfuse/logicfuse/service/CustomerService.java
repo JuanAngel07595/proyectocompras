4d0a13032129689c65da23ef6cbaf70382581552
Juan Angel <juanangel07595gmail.com>
2023-12-02 16:32:11 -0500

    Backend logindemo 9

--- src/main/java/com/logicfuse/logicfuse/service/CustomerService.java
+++ src/main/java/com/logicfuse/logicfuse/service/CustomerService.java
@@ -8,6 +8,8 @@ import com.logicfuse.logicfuse.models.LoginModel;
 import com.logicfuse.logicfuse.repositories.CustomerRepository;
 import com.logicfuse.logicfuse.repositories.LoginRepository;
 
+import javax.persistence.EntityNotFoundException;
+
 @Service
 public class CustomerService {
 
@@ -35,17 +37,27 @@ public class CustomerService {
     }
 
     public String login(LoginModel login) {
+        try{
         // Realizar lógica de autenticación aquí
         // Puedes utilizar Spring Security u otras técnicas según tus necesidades
 
         // Ejemplo simple: Verificar si el correo electrónico y la contraseña coinciden
         CustomerModel customer = customerRepository.findByEmail(login.getemail());
+            System.out.println("Email: " + login.getemail());
+            System.out.println("Contraseña: " + login.getCustomer().getContrasena());
 
         if (customer != null && customer.getContrasena().equals(login.getCustomer().getContrasena())) {
             return "Login exitoso";
         } else {
             throw new RuntimeException("Credenciales incorrectas");
         }
+        } catch (EntityNotFoundException e) {
+                e.printStackTrace(); // O utiliza un logger para registrar el error.
+                throw new RuntimeException("Usuario no encontrado");
+            } catch (RuntimeException e) {
+                e.printStackTrace(); // O utiliza un logger para registrar el error.
+                throw new RuntimeException("Error en el login: " + e.getMessage());
+            }
+        }
     }
-}
 
