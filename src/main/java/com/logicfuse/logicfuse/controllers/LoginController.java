427016ac002dbbdf7cf97fa095824d5d62c96a1a
Juan Angel <juanangel07595gmail.com>
2023-12-05 23:02:54 -0500

    Backend logindemo 18 complet

--- src/main/java/com/logicfuse/logicfuse/controllers/LoginController.java
+++ src/main/java/com/logicfuse/logicfuse/controllers/LoginController.java
@@ -5,8 +5,11 @@ import com.logicfuse.logicfuse.models.LoginModel;
 import com.logicfuse.logicfuse.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/login")
 public class LoginController {
 
 
