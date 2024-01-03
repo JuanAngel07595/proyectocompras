448f90ae41019748861a2e4b5eb282816de6e6a6
Juan Angel <juanangel07595gmail.com>
2024-01-03 15:50:35 -0500

    Implementación jwt Login 10

--- src/main/java/com/logicfuse/logicfuse/service/JwtService.java
+++ src/main/java/com/logicfuse/logicfuse/service/JwtService.java
@@ -5,12 +5,8 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
 
 import java.security.Key;
-
 import java.util.Date;
 
 @Service
@@ -22,29 +18,7 @@ public class JwtService {
     @Value("${jwt.expiration}")
     private Long expiration;
 
-    private Key secretKey = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS512);
-
-    public boolean isTokenExpired(String token) {
-        try {
-            Claims claims = Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody();
-
-            // Obtener la fecha de expiración del token
-            long expirationTimeMillis = claims.getExpiration().getTime();
-
-            // Obtener la fecha actual en milisegundos
-            long currentTimeMillis = System.currentTimeMillis();
-
-            // Verificar si el token ha expirado
-            return currentTimeMillis > expirationTimeMillis;
-        } catch (Exception e) {
-            // Manejar la excepción si ocurre algún problema durante la verificación del token
-            e.printStackTrace();
-            return true; // Tratamos como si el token está expirado para seguridad adicional
-        }
-    }
-
-
-
+    private Key secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
     public String generateToken(String email) {
         return Jwts.builder()
